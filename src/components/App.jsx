f43db88de4a3e66c54361d8080f90dcda5a0c58e
Isabelle <izzigrace6@gmail.com>
2022-08-08 17:47:06 -0700

    add buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,9 @@ import * as THREE from "three";
 import { GLTFLoader } from '../three.js-master/examples/jsm/loaders/GLTFLoader.js';
 import rubiksCube from '../models/rubikscube.gltf';
 import { OrbitControls } from '../three.js-master/examples/jsm/controls/OrbitControls.js';
-// import '../styles.css';
+import style from './App.module.css';
+import gradientLeft from '../pics/1.png';
+import gradientRight from '../pics/2.png';
 
 class App extends React.Component {
   constructor (props) {
@@ -15,10 +17,17 @@ class App extends React.Component {
 
   componentDidMount() {
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    scene.background = new THREE.Color( 0xffffff );
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
+
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    camera.position.set( 0, 15, 2 );
+    camera.lookAt(new THREE.Vector3(1,1,20))
+    // camera.position.z = 15;
+
     var controls = new OrbitControls( camera, renderer.domElement );
     controls.enableZoom = false;
     controls.enablePan = false;
@@ -28,19 +37,17 @@ class App extends React.Component {
     // controls.minAzimuthAngle = Math.PI / 2;
     // controls.maxAzimuthAngle = Math.PI / 2;
     controls.autoRotate = false;
-    camera.position.set( 0, 1, 2 );
 
-    scene.background = new THREE.Color( 0xffffff );
-    camera.position.z = 15;
-    controls.update();
+    // controls.update();
 
     const ambientLight = new THREE.AmbientLight( 0xffffff, 2 );
     // ambientLight.position.set(  0, 1, 2  );
     scene.add( ambientLight );
+    const directLight = new THREE.DirectionalLight(0xffffff, 2);
+    scene.add(directLight);
 
     const gltfLoader = new GLTFLoader();
     gltfLoader.load(rubiksCube, function(gltf) {
-      console.log('entered gltf loader');
       let model = gltf.scene;
       scene.add(model);
     });
@@ -55,8 +62,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={style.app} >
+        <div className={style.allButtons}>
+          <button className={style.shuffle}>SHUFFLE</button>
+          <button className={style.solve}>SOLVE</button>
+          <button className={style.startTimer}>START TIMER</button>
+        </div>
         <Cube />
+        <div>
+          <img src={gradientLeft} className={style.gradientLeftPic} alt="gradient background"></img>
+          <img src={gradientRight} className={style.gradientRightPic} alt="gradient background"></img>
+        </div>
       </div>
     )
   }
