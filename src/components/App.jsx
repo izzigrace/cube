606557c11199d2d854413e5a17fe31f14710b02a
Isabelle <izzigrace6@gmail.com>
2022-08-09 13:40:37 -0700

    get rotation on mouse scroll working:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Cube from './Cube.jsx';
 import style from './App.module.css';
 import gradientLeft from '../pics/1.png';
-import gradientRight from '../pics/2.png';
+// import gradientRight from '../pics/2.png';
 
 
 
@@ -24,7 +24,7 @@ class App extends React.Component {
         <Cube />
         <div>
           <img src={gradientLeft} className={style.gradientLeftPic} alt="gradient background"></img>
-          <img src={gradientRight} className={style.gradientRightPic} alt="gradient background"></img>
+          {/* <img src={gradientRight} className={style.gradientRightPic} alt="gradient background"></img> */}
         </div>
       </div>
     )
--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -44,8 +44,7 @@ class Cube extends React.Component {
     document.body.appendChild( renderer.domElement );
 
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    // camera.position.set( 30, 10, 2 );
-    camera.position.set( 0, 10, 2 );
+    camera.position.set( 0, 12, 2 );
     // camera.up.set(1, 1, 0);
     // camera.lookAt(new THREE.Vector3(0, 0, 0));
     // camera.position = new THREE.Vector3(-20, 20, 30);
@@ -60,93 +59,137 @@ class Cube extends React.Component {
     controls.maxPolarAngle = Math.PI / 2;
     controls.autoRotate = false;
 
+    //make lights
     const ambientLight = new THREE.AmbientLight( 0xffffff, 2 );
     scene.add( ambientLight );
     const directLight = new THREE.DirectionalLight(0xffffff, 2);
     directLight.position.set(  0, 1, 2  );
     scene.add(directLight);
+    //
 
+    const wholeCube = new THREE.Group();
     //start of rendering TWENTY SEVEN CUBES
     const gltfLoader = new GLTFLoader();
     gltfLoader.load(singlecube1, function(gltf) {
-      scene.add(gltf.scene);
+      let cube1 = gltf.scene;
+      // scene.add(cube1);
+      wholeCube.add(cube1);
     });
     gltfLoader.load(singlecube2, function(gltf) {
-      scene.add(gltf.scene);
+      let cube2 = gltf.scene;
+      wholeCube.add(cube2);
     });
     gltfLoader.load(singlecube3, function(gltf) {
-      scene.add(gltf.scene);
+      let cube3 = gltf.scene;
+      wholeCube.add(cube3);
     });
     gltfLoader.load(singlecube4, function(gltf) {
-      scene.add(gltf.scene);
+      let cube4 = gltf.scene;
+      wholeCube.add(cube4);
     });
     gltfLoader.load(singlecube5, function(gltf) {
-      scene.add(gltf.scene);
+      let cube5 = gltf.scene;
+      wholeCube.add(cube5);
     });
     gltfLoader.load(singlecube6, function(gltf) {
-      scene.add(gltf.scene);
+      let cube6 = gltf.scene;
+      wholeCube.add(cube6);
     });
     gltfLoader.load(singlecube7, function(gltf) {
-      scene.add(gltf.scene);
+      let cube7 = gltf.scene;
+      wholeCube.add(cube7);
     });
     gltfLoader.load(singlecube8, function(gltf) {
-      scene.add(gltf.scene);
+      let cube8 = gltf.scene;
+      wholeCube.add(cube8);
     });
     gltfLoader.load(singlecube9, function(gltf) {
-      scene.add(gltf.scene);
+      let cube9 = gltf.scene;
+      wholeCube.add(cube9);
     });
     gltfLoader.load(singlecube10, function(gltf) {
-      scene.add(gltf.scene);
+      let cube10 = gltf.scene;
+      wholeCube.add(cube10);
     });
     gltfLoader.load(singlecube11, function(gltf) {
-      scene.add(gltf.scene);
+      let cube11 = gltf.scene;
+      wholeCube.add(cube11);
     });
     gltfLoader.load(singlecube12, function(gltf) {
-      scene.add(gltf.scene);
+      let cube12 = gltf.scene;
+      wholeCube.add(cube12);
     });
     gltfLoader.load(singlecube13, function(gltf) {
-      scene.add(gltf.scene);
+      let cube13 = gltf.scene;
+      wholeCube.add(cube13);
     });
     gltfLoader.load(singlecube14, function(gltf) {
-      scene.add(gltf.scene);
+      let cube14 = gltf.scene;
+      wholeCube.add(cube14);
     });
     gltfLoader.load(singlecube15, function(gltf) {
-      scene.add(gltf.scene);
+      let cube15 = gltf.scene;
+      wholeCube.add(cube15);
     });
     gltfLoader.load(singlecube16, function(gltf) {
-      scene.add(gltf.scene);
+      let cube16 = gltf.scene;
+      wholeCube.add(cube16);
     });
     gltfLoader.load(singlecube17, function(gltf) {
-      scene.add(gltf.scene);
+      let cube17 = gltf.scene;
+      wholeCube.add(cube17);
     });
     gltfLoader.load(singlecube18, function(gltf) {
-      scene.add(gltf.scene);
+      let cube18 = gltf.scene;
+      wholeCube.add(cube18);
     });
     gltfLoader.load(singlecube19, function(gltf) {
-      scene.add(gltf.scene);
+      let cube19 = gltf.scene;
+      wholeCube.add(cube19);
     });
     gltfLoader.load(singlecube20, function(gltf) {
-      scene.add(gltf.scene);
+      let cube20 = gltf.scene;
+      wholeCube.add(cube20);
     });
     gltfLoader.load(singlecube21, function(gltf) {
-      scene.add(gltf.scene);
+      let cube21 = gltf.scene;
+      wholeCube.add(cube21);
     });
     gltfLoader.load(singlecube22, function(gltf) {
-      scene.add(gltf.scene);
+      let cube22 = gltf.scene;
+      wholeCube.add(cube22);
     });
     gltfLoader.load(singlecube23, function(gltf) {
-      scene.add(gltf.scene);
+      let cube23 = gltf.scene;
+      wholeCube.add(cube23);
     });
     gltfLoader.load(singlecube24, function(gltf) {
-      scene.add(gltf.scene);
+      let cube24 = gltf.scene;
+      wholeCube.add(cube24);
     });
     gltfLoader.load(singlecube25, function(gltf) {
-      scene.add(gltf.scene);
+      let cube25 = gltf.scene;
+      wholeCube.add(cube25);
     });
     gltfLoader.load(singlecube26, function(gltf) {
-      scene.add(gltf.scene);
+      let cube26 = gltf.scene;
+      wholeCube.add(cube26);
     });
     //ending finally
+    scene.add(wholeCube);
+    wholeCube.position.set(-2, 0, -2);
+
+    console.log(scene.children);
+    // wholeCube.translate( 0, 0, 0 );
+    // wholeCube.center();
+
+    window.addEventListener('wheel', onMouseWheel, false);
+    function onMouseWheel (event) {
+      event.preventDefault();
+      wholeCube.rotation.y += event.deltaX * 0.01;
+      wholeCube.rotation.x += event.deltaY * 0.01;
+      // camera.rotation.y -= event.deltaY * 0.00005;
+    }
 
     function animate() {
       requestAnimationFrame( animate );
