b76595679375076b6c6ec9383c0bdf40e42e4d97
Isabelle <izzigrace6@gmail.com>
2022-08-10 23:01:51 -0700

    almost get rotation working

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -90,38 +90,6 @@ class Cube extends React.Component {
     // } sadly had to get rid of this
 
 
-  //   function getContainerObjByChild (child) {
-  //     if (child.userData.isContainer) {
-  //       return child;
-  //     }
-
-  //     else if(child.parent != null) {
-  //       return getContainerObjByChild(child.parent);
-  //     }
-
-  //     else {
-  //       return null;
-  //     }
-  //  }
-
-  function isCursorOnCube (cursorX, cursorY) {
-    var directionVector = new THREE.Vector3();
-
-    //Normalise mouse x and y
-    var x = (cursorX / (window.innerWidth)|0) * 2 - 1;
-    var y = -(cursorY / (window.innerHeight)|0) * 2 + 1;
-
-    directionVector.set(x, y, 1);
-
-    // projector.unprojectVector(directionVector, camera);
-    directionVector.sub(camera.position);
-    directionVector.normalize();
-    raycaster.set(camera.position, directionVector);
-
-    return raycaster.intersectObjects(wholeCube, true).length > 0;
-  }
-
-
   const wholeCubeRaycaster = new THREE.Raycaster();
 
   var mouseOnCube = false;
@@ -129,11 +97,13 @@ class Cube extends React.Component {
   var leftOrRight = '';
   var upOrDown = '';
   var upDownLeftOrRight = '';
+  var dontDoMouseUp = false;
 
   window.addEventListener('mousemove', (event) => {
     //seeing if mouse is over cube, disabling orbit controls if it is
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
     wholeCubeRaycaster.setFromCamera(mouse, camera);
     const intersectsCube = wholeCubeRaycaster.intersectObject(scene);
     if (intersectsCube.length === 0) {
@@ -176,10 +146,11 @@ class Cube extends React.Component {
     var xAxisGroup = [];
 
     window.addEventListener('mousedown', (event) => {
-      if (mouseOnCube) {
+      xAxisGroup = [];
+      yAxisGroup = [];
 
-        xAxisGroup = [];
-        yAxisGroup = [];
+      if (mouseOnCube) {
+        dontDoMouseUp = false;
 
         //set mouse location
         mouseLocOnDown.x = event.clientX;
@@ -198,8 +169,8 @@ class Cube extends React.Component {
       // controls.enabled = false;
       // console.log(scene.children);
 
-      xAxisGroup.push(intersects[0].object.parent);
-      yAxisGroup.push(intersects[0].object.parent);
+      // xAxisGroup.push(intersects[0].object.parent);
+      // yAxisGroup.push(intersects[0].object.parent);
 
       var facePoint = intersects[0].point;
 
@@ -225,7 +196,7 @@ class Cube extends React.Component {
         yAxisGroup.push(intersects2[i].object.parent);
         // var raycaster3
       }
-      // console.log(intersects[0].face.normal);
+
 
       //make raycaster for each cube that was selected from last raycaster,  Y AXIS CUBES
       var raycaster3 = new THREE.Raycaster();
@@ -252,6 +223,7 @@ class Cube extends React.Component {
         yAxisGroup.push(intersects5[i].object.parent);
       }
 
+
       //make raycaster for each cube that was selected from last raycaster,  X AXIS CUBES
       var raycaster6 = new THREE.Raycaster();
       raycaster6.ray.origin.copy(intersects2[0].point);
@@ -277,6 +249,8 @@ class Cube extends React.Component {
         xAxisGroup.push(intersects8[i].object.parent);
       }
 
+    } else {
+      dontDoMouseUp = true;
     }
     console.log(xAxisGroup);
     console.log(yAxisGroup);
@@ -288,10 +262,26 @@ class Cube extends React.Component {
     rotateGroup.position.set(0, 1, 0);
     var rotationTargetGroup = new THREE.Group();
     rotationTargetGroup.position.set(0, 1, 0);
+    scene.add(rotationTargetGroup);
     var isAnimating = false;
 
     window.addEventListener('mouseup', (event) => {
+      console.log(dontDoMouseUp);
+      if (!dontDoMouseUp) {
+      if (isAnimating) {
+        return;
+      }
+      setTimeout(() => {
+        isAnimating = false;
+        while (rotateGroup.children.length) {
+          wholeCube.attach(rotateGroup.children[0]);
+        }
+        rotationTargetGroup = new THREE.Group();
+        rotationTargetGroup.position.set(0, 1, 0);
+        scene.add(rotationTargetGroup);
+      }, 900)
       console.log(upDownLeftOrRight);
+
       if (upDownLeftOrRight === 'right') {
         for (let i = 0; i < xAxisGroup.length; i++) {
           rotateGroup.attach(xAxisGroup[i]);
@@ -321,6 +311,9 @@ class Cube extends React.Component {
         rotationTargetGroup.rotation.x += PI / 2;
         isAnimating = true;
       }
+
+    }
+
     })
 
 
@@ -328,110 +321,113 @@ class Cube extends React.Component {
     const wholeCube = new THREE.Group();
     //start of rendering TWENTY SEVEN CUBES
     const gltfLoader = new GLTFLoader();
-    gltfLoader.load(singlecube1, function(gltf) {
-      let cube1 = gltf.scene;
-      wholeCube.add(cube1);
-    });
-    gltfLoader.load(singlecube2, function(gltf) {
-      let cube2 = gltf.scene;
-      wholeCube.add(cube2);
-    });
-    gltfLoader.load(singlecube3, function(gltf) {
-      let cube3 = gltf.scene;
-      wholeCube.add(cube3);
-    });
-    gltfLoader.load(singlecube4, function(gltf) {
-      let cube4 = gltf.scene;
-      wholeCube.add(cube4);
-    });
-    gltfLoader.load(singlecube5, function(gltf) {
-      let cube5 = gltf.scene;
-      wholeCube.add(cube5);
-    });
-    gltfLoader.load(singlecube6, function(gltf) {
-      let cube6 = gltf.scene;
-      wholeCube.add(cube6);
-    });
-    gltfLoader.load(singlecube7, function(gltf) {
-      let cube7 = gltf.scene;
-      wholeCube.add(cube7);
-    });
-    gltfLoader.load(singlecube8, function(gltf) {
-      let cube8 = gltf.scene;
-      wholeCube.add(cube8);
-    });
-    gltfLoader.load(singlecube9, function(gltf) {
-      let cube9 = gltf.scene;
-      wholeCube.add(cube9);
-    });
-    gltfLoader.load(singlecube10, function(gltf) {
-      let cube10 = gltf.scene;
-      wholeCube.add(cube10);
-    });
-    gltfLoader.load(singlecube11, function(gltf) {
-      let cube11 = gltf.scene;
-      wholeCube.add(cube11);
-    });
-    gltfLoader.load(singlecube12, function(gltf) {
-      let cube12 = gltf.scene;
-      wholeCube.add(cube12);
-    });
-    gltfLoader.load(singlecube13, function(gltf) {
-      let cube13 = gltf.scene;
-      wholeCube.add(cube13);
-    });
-    gltfLoader.load(singlecube14, function(gltf) {
-      let cube14 = gltf.scene;
-      wholeCube.add(cube14);
-    });
-    gltfLoader.load(singlecube15, function(gltf) {
-      let cube15 = gltf.scene;
-      wholeCube.add(cube15);
-    });
-    gltfLoader.load(singlecube16, function(gltf) {
-      let cube16 = gltf.scene;
-      wholeCube.add(cube16);
-    });
-    gltfLoader.load(singlecube17, function(gltf) {
-      let cube17 = gltf.scene;
-      wholeCube.add(cube17);
-    });
-    gltfLoader.load(singlecube18, function(gltf) {
-      let cube18 = gltf.scene;
-      wholeCube.add(cube18);
-    });
-    gltfLoader.load(singlecube19, function(gltf) {
-      let cube19 = gltf.scene;
-      wholeCube.add(cube19);
-    });
-    gltfLoader.load(singlecube20, function(gltf) {
-      let cube20 = gltf.scene;
-      wholeCube.add(cube20);
-    });
-    gltfLoader.load(singlecube21, function(gltf) {
-      let cube21 = gltf.scene;
-      wholeCube.add(cube21);
-    });
-    gltfLoader.load(singlecube22, function(gltf) {
-      let cube22 = gltf.scene;
-      wholeCube.add(cube22);
-    });
-    gltfLoader.load(singlecube23, function(gltf) {
-      let cube23 = gltf.scene;
-      wholeCube.add(cube23);
-    });
-    gltfLoader.load(singlecube24, function(gltf) {
-      let cube24 = gltf.scene;
-      wholeCube.add(cube24);
-    });
-    gltfLoader.load(singlecube25, function(gltf) {
-      let cube25 = gltf.scene;
-      wholeCube.add(cube25);
-    });
-    gltfLoader.load(singlecube26, function(gltf) {
-      let cube26 = gltf.scene;
-      wholeCube.add(cube26);
-    });
+    function loadGltfs () {
+      gltfLoader.load(singlecube1, function(gltf) {
+        let cube1 = gltf.scene;
+        wholeCube.add(cube1);
+      });
+      gltfLoader.load(singlecube2, function(gltf) {
+        let cube2 = gltf.scene;
+        wholeCube.add(cube2);
+      });
+      gltfLoader.load(singlecube3, function(gltf) {
+        let cube3 = gltf.scene;
+        wholeCube.add(cube3);
+      });
+      gltfLoader.load(singlecube4, function(gltf) {
+        let cube4 = gltf.scene;
+        wholeCube.add(cube4);
+      });
+      gltfLoader.load(singlecube5, function(gltf) {
+        let cube5 = gltf.scene;
+        wholeCube.add(cube5);
+      });
+      gltfLoader.load(singlecube6, function(gltf) {
+        let cube6 = gltf.scene;
+        wholeCube.add(cube6);
+      });
+      gltfLoader.load(singlecube7, function(gltf) {
+        let cube7 = gltf.scene;
+        wholeCube.add(cube7);
+      });
+      gltfLoader.load(singlecube8, function(gltf) {
+        let cube8 = gltf.scene;
+        wholeCube.add(cube8);
+      });
+      gltfLoader.load(singlecube9, function(gltf) {
+        let cube9 = gltf.scene;
+        wholeCube.add(cube9);
+      });
+      gltfLoader.load(singlecube10, function(gltf) {
+        let cube10 = gltf.scene;
+        wholeCube.add(cube10);
+      });
+      gltfLoader.load(singlecube11, function(gltf) {
+        let cube11 = gltf.scene;
+        wholeCube.add(cube11);
+      });
+      gltfLoader.load(singlecube12, function(gltf) {
+        let cube12 = gltf.scene;
+        wholeCube.add(cube12);
+      });
+      gltfLoader.load(singlecube13, function(gltf) {
+        let cube13 = gltf.scene;
+        wholeCube.add(cube13);
+      });
+      gltfLoader.load(singlecube14, function(gltf) {
+        let cube14 = gltf.scene;
+        wholeCube.add(cube14);
+      });
+      gltfLoader.load(singlecube15, function(gltf) {
+        let cube15 = gltf.scene;
+        wholeCube.add(cube15);
+      });
+      gltfLoader.load(singlecube16, function(gltf) {
+        let cube16 = gltf.scene;
+        wholeCube.add(cube16);
+      });
+      gltfLoader.load(singlecube17, function(gltf) {
+        let cube17 = gltf.scene;
+        wholeCube.add(cube17);
+      });
+      gltfLoader.load(singlecube18, function(gltf) {
+        let cube18 = gltf.scene;
+        wholeCube.add(cube18);
+      });
+      gltfLoader.load(singlecube19, function(gltf) {
+        let cube19 = gltf.scene;
+        wholeCube.add(cube19);
+      });
+      gltfLoader.load(singlecube20, function(gltf) {
+        let cube20 = gltf.scene;
+        wholeCube.add(cube20);
+      });
+      gltfLoader.load(singlecube21, function(gltf) {
+        let cube21 = gltf.scene;
+        wholeCube.add(cube21);
+      });
+      gltfLoader.load(singlecube22, function(gltf) {
+        let cube22 = gltf.scene;
+        wholeCube.add(cube22);
+      });
+      gltfLoader.load(singlecube23, function(gltf) {
+        let cube23 = gltf.scene;
+        wholeCube.add(cube23);
+      });
+      gltfLoader.load(singlecube24, function(gltf) {
+        let cube24 = gltf.scene;
+        wholeCube.add(cube24);
+      });
+      gltfLoader.load(singlecube25, function(gltf) {
+        let cube25 = gltf.scene;
+        wholeCube.add(cube25);
+      });
+      gltfLoader.load(singlecube26, function(gltf) {
+        let cube26 = gltf.scene;
+        wholeCube.add(cube26);
+      });
+    }
+    loadGltfs();
     //ending mini cubes finally and adding group of all of them to scene
     scene.add(wholeCube);
     wholeCube.position.set(0, 1, 0);
@@ -441,6 +437,10 @@ class Cube extends React.Component {
       requestAnimationFrame( animate );
       if (isAnimating) {
         rotateGroup.quaternion.slerp(rotationTargetGroup.quaternion, 0.1);
+      } else {
+        while (rotateGroup.children.length) {
+          wholeCube.attach(rotateGroup.children[0]);
+        }
       }
 
       controls.update();
